55df4e60436230b15a6180b95c79e83ec2be02a2
JadoGarBhai <sikandernawaz35@gmail.com>
2024-03-10 21:56:23 +0500

    dropdown

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { ThemeToggler } from "./ThemeToggler";
+import SearchInput from "./SearchInput";
 
 function Header() {
   return (
@@ -16,6 +17,7 @@ function Header() {
       </Link>
 
       <div className="flex space-x-2">
+        <SearchInput />
         <ThemeToggler />
       </div>
     </div>
