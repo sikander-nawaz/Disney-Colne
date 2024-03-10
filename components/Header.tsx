fef057f4e10b972ed81532960c25b7af791ac8b6
JadoGarBhai <sikandernawaz35@gmail.com>
2024-03-10 22:54:33 +0500

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,10 +2,11 @@ import Image from "next/image";
 import Link from "next/link";
 import { ThemeToggler } from "./ThemeToggler";
 import SearchInput from "./SearchInput";
+import GenreDropdown from "./GenreDropdown";
 
 function Header() {
   return (
-    <div>
+    <header className="fixed w-full z-20 top-0 flex items-center justify-between p-5 bg-gradient-to-t from-gray-200/0 via-gray-900/25 to-gray-900">
       <Link href="/" className="mr-10">
         <Image
           src="https://links.papareact.com/a943ae"
@@ -17,10 +18,11 @@ function Header() {
       </Link>
 
       <div className="flex space-x-2">
+        <GenreDropdown />
         <SearchInput />
         <ThemeToggler />
       </div>
-    </div>
+    </header>
   );
 }
 
