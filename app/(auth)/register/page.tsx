69b811e480fef33e5c095a2daa43b6a42f77419d
JadoGarBhai <sikandernawaz35@gmail.com>
2024-04-01 23:04:02 +0500

    signup/databade

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 import { toast } from "react-hot-toast";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
@@ -14,14 +15,59 @@ const Register = () => {
     confirmPassword: string;
   }
 
+  const [data, setData] = useState<Data>({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const router = useRouter();
   const [isPasswordShow, setIsPasswordShow] = useState(false);
   const [isConfirmPasswordShow, setIsConfirmPasswordShow] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleSubmit = () => {
-    setIsLoading(false);
-    toast.error("You did it!");
+  const handleChange = (e: any) => {
+    setData({ ...data, [e.target.name]: e.target.value });
   };
+
+  const handleSubmit = async () => {
+    setIsLoading(true);
+    const email = data.email;
+    const password = data.password;
+    const confirmPassword = data.confirmPassword;
+
+    try {
+      if (email == "") {
+        toast.error("Please enter your email");
+      } else if (!email.includes("@")) {
+        toast.error("Please enter valid email");
+      } else if (password.length < 8) {
+        toast.error("Password should be at leaste 8 characters");
+      } else if (password !== confirmPassword) {
+        toast.error("Please correct your confirmation password");
+      } else {
+        const response = await fetch("/api/register", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email, password }),
+        });
+
+        const res = await response.json();
+        console.log(res);
+        if (response.status == 200) {
+          toast.success("User Registered Successfully");
+          router.push("/login");
+        }
+        if (response.status == 400) {
+          toast.error(res.msg);
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    setIsLoading(false);
+  };
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-between pt-20">
       <div className="p-8 rounded-lg shadow-lg w-[25rem]">
@@ -37,7 +83,7 @@ const Register = () => {
           className="w-full border border-gray-300 text-black rounded px-3 py-2 mb-2 focus:outline-none focus:border-blue-400 focus:text-black text-[18px]"
           placeholder="Email"
           required
-          // onChange={handleChange}
+          onChange={handleChange}
         />
 
         <div className="flex">
@@ -47,7 +93,7 @@ const Register = () => {
             className="w-full border border-gray-300 text-black rounded-s mb-2 px-3 py-2 focus:outline-none focus:border-blue-400 focus:text-black text-[18px]"
             placeholder="Password should be 8 characters"
             required
-            // onChange={handleChange}
+            onChange={handleChange}
           />
 
           <button
@@ -68,12 +114,12 @@ const Register = () => {
 
         <div className="flex">
           <input
-            name="password"
+            name="confirmPassword"
             type={isConfirmPasswordShow ? "text" : "password"}
             className="w-full border border-gray-300 text-black rounded-s px-3 py-2 focus:outline-none focus:border-blue-400 focus:text-black text-[18px]"
             placeholder="Confirm Password"
             required
-            // onChange={handleChange}
+            onChange={handleChange}
           />
 
           <button
@@ -98,7 +144,16 @@ const Register = () => {
             disabled={isLoading}
             className=" w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 mt-6 text-[16px]"
           >
-            {!isLoading ? "Hehehe" : "Hohoho"}
+            {!isLoading ? (
+              "Register"
+            ) : (
+              <>
+                <svg
+                  className="animate-spin h-5 w-5 mr-3 text-white"
+                  viewBox="0 0 24 24"
+                ></svg>
+              </>
+            )}
           </button>
         </div>
 
